eca09d47fd676696c7ee2917e7500130160a7116
Swapnil-DevGeek <f20220181@goa.bits-pilani.ac.in>
2024-06-11 13:36:44 +0530

    landing page completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/navbar/Navbar'
 import Categories from './components/categorySection/Categories'
 import Drone from './components/droneCards/Drone'
 import Propeller from './components/buyPropeller/Propeller'
+import Controller from './components/buyControllers/Controller'
+import Camera from './components/buyCameras/Camera'
+import Battery from './components/buyBatteries/Battery'
 
 const App = () => {
   return (
@@ -10,7 +13,10 @@ const App = () => {
       <Navbar/>
       <Categories/>
       <Drone/>
+      <Battery/>
       <Propeller/>
+      <Camera/>
+      <Controller/>
     </>
   )
 }
